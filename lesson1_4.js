442271f14c2b058a6e60be334ad058c48d44ac9f
s-k-git <89926320+s-k-git@users.noreply.github.com>
2021-09-07 14:45:25 +0900

    alertでの追加通知を入れました

--- lesson1_4.js
+++ lesson1_4.js
@@ -1,17 +1,25 @@
 const tasks = ["[内容]机を片付ける、[ジャンル]掃除", "[内容]牛乳を買う、[ジャンル]買い物", "[内容]散歩する、[ジャンル]運動"];
+console.log("=============================");
+console.log("現在持っているタスク一覧");
+console.log("=============================");
+tasks.forEach(function (value, index) {
+    console.log(index + ":" + value);
+    }
+);
 while (true) {
-    console.log("=============================");
-    console.log("現在持っているタスク一覧");
-    console.log("=============================");
-    tasks.forEach(function (value, index) {
-        console.log(index + ":" + value);
-        }
-    );
     const task = prompt("タスクを入力してください");
     const genre = prompt("ジャンルを入力してください");
     if (task === null && genre === null) {
         break;
     } else {
         tasks.push("[内容]" + task + "[ジャンル]" + genre);
+        console.log("=============================");
+        console.log("現在持っているタスク一覧");
+        console.log("=============================");
+        tasks.forEach(function (value, index) {
+            console.log(index + ":" + value);
+        }
+        );
+        alert("新しいタスクを追加しました");
     }
 }
