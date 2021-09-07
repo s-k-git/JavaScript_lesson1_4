6d5e5a8440fc4fef22dadfd79598c182deab918d
s-k-git <89926320+s-k-git@users.noreply.github.com>
2021-09-07 18:34:52 +0900

    確認,追加,削除,終了の導入

--- lesson1_4.js
+++ lesson1_4.js
@@ -1,10 +1,10 @@
-const tasks = ["[内容]机を片付ける、[ジャンル]掃除", "[内容]牛乳を買う、[ジャンル]買い物", "[内容]散歩する、[ジャンル]運動"];
+const tasks = ["[内容]机を片付ける[ジャンル]掃除", "[内容]牛乳を買う[ジャンル]買い物", "[内容]散歩する[ジャンル]運動"];
 console.log("=============================");
 console.log("現在持っているタスク一覧");
 console.log("=============================");
 tasks.forEach(function (value, index) {
     console.log(index + ":" + value);
-    }
+}
 );
 while (true) {
     const task = prompt("タスクを入力してください");
@@ -21,5 +21,41 @@ while (true) {
         }
         );
         alert("新しいタスクを追加しました");
+        let action = prompt("「確認,追加,削除,終了」の4つのいずれかを入力してください");//constは不可
+        while (true) {
+            if (action === "追加" || action === "終了" || action === null) {
+                break;
+            } else if (action === "確認") {
+                alert("=============================\r\n現在持っているタスク一覧\r\n=============================\r\n"
+                    + tasks);
+                action = prompt("「確認,追加,削除,終了」の4つのいずれかを入力してください");
+            } else if (action === "削除") {
+                const deleteNo = prompt("削除する要素番号を記入してください");
+                if (deleteNo === null || deleteNo === "") {
+                    alert("キャンセル");
+                } else if (Number.isInteger(Number(deleteNo)) && 0 <= deleteNo && deleteNo < tasks.length) {
+                    alert("true");
+                    tasks.splice(deleteNo, 1);
+                    console.log("=============================");
+                    console.log("現在持っているタスク一覧");
+                    console.log("=============================");
+                    tasks.forEach(function (value, index) {
+                        console.log(index + ":" + value);
+                    }
+                    );
+                } else {
+                    alert("該当要素番号がありません");
+                }
+                action = prompt("「確認,追加,削除,終了」の4つのいずれかを入力してください");
+            } else {
+                action = prompt("「確認,追加,削除,終了」の4つのいずれかを入力してください");
+            }
+        }
+        if (action === "終了" || action === null) {
+            alert("終了します");
+            break;
+        } else if (action === "追加") {
+            alert("追加するタスクを入力してください");
+        }
     }
 }
